4b27b505350030703d421876670e1c56cb96d8dd
Rock <739965647@qq.com>
2019-08-21 09:54:24 +0800

    修改了get_value.py中的文件描述

--- common/get_value.py
+++ common/get_value.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/8/20 16:53
 # @Author  : DannyDong
-# @File    : base_test.py
-# @describe: 测试用例基类
+# @File    : get_value.py
+# @describe: 配置文件读取
 
 from common.read_config import ReadIni
 from common.utils import create_name, create_phone
