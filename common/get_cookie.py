c8d167c6cbfcae1cff315f325c32193f558757fe
Rock <739965647@qq.com>
2019-08-23 11:04:55 +0800

    增加了校区切换，避免测试数据污染其他校区

--- common/get_cookie.py
+++ common/get_cookie.py
@@ -6,16 +6,20 @@
 
 import requests
 from common.get_value import GetValue
+from common.get_log import TestLogs
 
 
 class LoginApi(GetValue):
     def __init__(self):
         self.url = self.base_url + "manager/signin"
+        self.url2 = self.base_url + "manager/switch_campus"
+        self.log = TestLogs().get_log()
 
     def get_cookie(self):
         r = requests.post(self.url, data={"userName": self.username, "password": self.password})
         login_cookie = r.cookies
-        # print(login_cookie)
+        # 调用校区切换接口，保证在测试校区运行测试，避免测试数据污染
+        requests.post(self.url2, data={"campusId": "N89r0ljVIIw0"}, cookies=login_cookie)
         return login_cookie
 
 
--- common/read_config.py
+++ common/read_config.py
@@ -4,50 +4,6 @@
 # @File    : read_config.py
 # @describe: 读取配置文件
 
-# import os
-# import codecs
-# import configparser
-#
-# proDir = os.path.split(os.path.realpath(__file__))[0]
-# configPath = os.path.join(proDir, "config.ini")
-#
-#
-# class ReadConfig:
-#     def __init__(self):
-#         fd = open(configPath)
-#         data = fd.read()
-#
-#         #  remove BOM
-#         if data[:3] == codecs.BOM_UTF8:
-#             data = data[3:]
-#             file = codecs.open(configPath, "w")
-#             file.write(data)
-#             file.close()
-#         fd.close()
-#
-#         self.cf = configparser.ConfigParser()
-#         self.cf.read(configPath)
-#
-#     def get_email(self, name):
-#         value = self.cf.get("EMAIL", name)
-#         return value
-#
-#     def get_http(self, name):
-#         value = self.cf.get("HTTP", name)
-#         return value
-#
-#     def get_login(self, name):
-#         value = self.cf.get("LOGIN", name)
-#         return value
-#
-#
-# if __name__ == '__main__':
-#     demo = ReadConfig()
-#     print(demo.get_email("mail_pass"))
-#     print(demo.get_http("base_url"))
-#     print(demo.get_login("userName"))
-
-
 import configparser
 
 
